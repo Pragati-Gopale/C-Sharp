de97d52ff9128cf7dd00b982926c473cbb576b83
Pragati-Gopale <116192460+Pragati-Gopale@users.noreply.github.com>
2024-03-05 09:36:28 +0530

    Add files via upload

---
+++ Structure/Structure/CodeFile1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Structure
+{
+    struct Structure1  //same as class
+    {
+        int i;  //cannot initialize field at the time of declaration
+
+        public Structure1(int i)
+        {
+            this.i = i;
+        }
+
+
+        void display()
+        {
+            Console.WriteLine("Method is called in structure." + i);
+        }
+
+        static void Main(string[] args)
+        {
+            Structure1 a = new Structure1(); //with instance creation (default value assign to field)
+            a.i = 1;
+            a.display();
+
+            Structure1 b;  //without instance creation
+            b.i = 2;
+            b.display();
+
+            Structure1 c = new Structure1(3); //by using parameterized constructor 
+            c.display();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Structure/Structure/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure
+{
+   
+    class Program
+    {
+        int i = 1; //can initialize field at the time of declaration
+        public Program(int i) 
+        {
+            this.i = i;
+        }
+
+        
+
+        void display()
+        {
+            Console.WriteLine("Method is called in class:" + i);
+        }
+        static void Main(string[] args)
+        {
+            /* Program m = new Program();  1. by using default constructor
+               m.display();  
+            */
+
+            Program n =new Program(1);  //2. by using parameterized constructor
+            n.display();
+            
+
+            Console.ReadLine();
+        }
+    }
+}
